3040fc53a1b0f893b344a2759f324a1db0fd015a
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-05-30 19:42:28 -0400

    Enable method-level security in SecurityConfig to enhance access control

--- user-service/src/main/java/com/rajeswaran/user/SecurityConfig.java
+++ user-service/src/main/java/com/rajeswaran/user/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.rajeswaran.user;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
