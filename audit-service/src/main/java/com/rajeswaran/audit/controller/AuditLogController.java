60e908ef89829831dccceba56d2934b7ced7548e
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-05-31 08:09:39 -0400

    Enhance security in AuditLogController and configure JWT authentication in SecurityConfig

--- audit-service/src/main/java/com/rajeswaran/audit/controller/AuditLogController.java
+++ audit-service/src/main/java/com/rajeswaran/audit/controller/AuditLogController.java
@@ -5,6 +5,7 @@ import com.rajeswaran.audit.service.AuditLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.Optional;
 @Slf4j
 @RestController
 @RequestMapping("/api/audit-logs")
+@PreAuthorize("hasRole(T(com.rajeswaran.common.AppConstants).ROLE_BAAS_ADMIN)")
 public class AuditLogController {
     @Autowired
     private AuditLogService auditLogService;
@@ -40,5 +42,4 @@ public class AuditLogController {
             return ResponseEntity.notFound().build();
         }
     }
-
 }
