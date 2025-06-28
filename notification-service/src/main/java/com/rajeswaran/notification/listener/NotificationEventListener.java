21799db2f3fe5094e07f938fc15f71996adb1b2b
Rajeswaran Dhandpani <prorajeswaran@gmail.com>
2025-06-28 16:48:44 +0000

    feat: add NotificationEventListener for handling account and payment notifications

--- notification-service/src/main/java/com/rajeswaran/notification/listener/NotificationEventListener.java
+++ notification-service/src/main/java/com/rajeswaran/notification/listener/NotificationEventListener.java
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
 
 @Slf4j
 @Component
-public class notificationEventListener {    
+public class NotificationEventListener {
     @Autowired
     private NotificationService notificationService;
 
